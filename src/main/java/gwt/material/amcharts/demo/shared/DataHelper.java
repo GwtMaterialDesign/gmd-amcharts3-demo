112c6aee54ca56c25cead2d6ab311768c26fb6b3
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2018-03-17 06:59:39 +0800

    added more charts

--- src/main/java/gwt/material/amcharts/demo/shared/DataHelper.java
+++ src/main/java/gwt/material/amcharts/demo/shared/DataHelper.java
@@ -4,6 +4,7 @@ import gwt.material.amcharts.demo.shared.model.ChartData;
 import gwt.material.amcharts.demo.shared.model.bar.*;
 import gwt.material.amcharts.demo.shared.model.column.*;
 import gwt.material.amcharts.demo.shared.model.line.DateBasedData;
+import gwt.material.amcharts.demo.shared.model.polar.PolarChart;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +27,7 @@ public class DataHelper {
         list.add(new FloatingBarChart());
         list.add(new ColumnImageChart());
         list.add(new DateBasedData());
+        list.add(new PolarChart());
         return list;
     }
 }
