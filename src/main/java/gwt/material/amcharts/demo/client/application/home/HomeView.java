23748491eef5eeb2b57f2e450480d0fa5e2490f8
mark.baldemor <kevzlou7979@gmail.com>
2018-08-21 13:55:28 +0800

    Working - Scrollbar & LineSeries

--- src/main/java/gwt/material/amcharts/demo/client/application/home/HomeView.java
+++ src/main/java/gwt/material/amcharts/demo/client/application/home/HomeView.java
@@ -20,20 +20,30 @@
 package gwt.material.amcharts.demo.client.application.home;
 
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 import gwt.material.design.amcharts.client.ui.Am4Charts;
 import gwt.material.design.amcharts.client.ui.Am4Core;
 import gwt.material.design.amcharts.client.ui.chart.PieChart;
+import gwt.material.design.amcharts.client.ui.chart.XYChart;
+import gwt.material.design.amcharts.client.ui.chart.axis.CategoryAxis;
+import gwt.material.design.amcharts.client.ui.chart.axis.ValueAxis;
 import gwt.material.design.amcharts.client.ui.chart.resources.ChartClientBundle;
+import gwt.material.design.amcharts.client.ui.chart.scrollbar.XYChartScrollbar;
+import gwt.material.design.amcharts.client.ui.chart.series.LineSeries;
 import gwt.material.design.amcharts.client.ui.chart.series.PieSeries;
 import gwt.material.design.client.MaterialDesignBase;
+import gwt.material.design.client.ui.MaterialCard;
 
 import javax.inject.Inject;
 
 public class HomeView extends ViewImpl implements HomePresenter.MyView {
     interface Binder extends UiBinder<Widget, HomeView> {
     }
+
+    @UiField
+    MaterialCard pieChartPanel, xyChartPanel;
 
     @Inject
     HomeView(Binder uiBinder) {
@@ -43,16 +53,37 @@ public class HomeView extends ViewImpl implements HomePresenter.MyView {
     @Override
     protected void onAttach() {
         super.onAttach();
-        
+
+        // Inject the necessary javascript resources
         MaterialDesignBase.injectDebugJs(ChartClientBundle.INSTANCE.coreJs());
         MaterialDesignBase.injectDebugJs(ChartClientBundle.INSTANCE.chartsJs());
-        MaterialDesignBase.injectDebugJs(ChartClientBundle.INSTANCE.animatedJs());
 
-        PieChart chart = (PieChart) Am4Core.create("div", Am4Charts.PieChart);
-
-        PieSeries series = chart.series.push(new PieSeries());
+        // Pie Chart Demo
+        PieChart pieChart = (PieChart) Am4Core.create(pieChartPanel, Am4Charts.PieChart);
+        PieSeries series = pieChart.series.push(new PieSeries());
         series.dataFields.value = "litres";
         series.dataFields.category = "country";
-        chart.dataSource.url = "data/basic.json";
+        pieChart.dataSource.url = "data/basic.json";
+
+        // XYChart Demo
+        XYChart xyChart = (XYChart) Am4Core.create(xyChartPanel, Am4Charts.XYChart);
+        CategoryAxis categoryAxis = (CategoryAxis) xyChart.xAxes.push(new CategoryAxis());
+        categoryAxis.dataFields.category = "country";
+        categoryAxis.title.text = "Countries";
+
+        ValueAxis valueAxis = (ValueAxis) xyChart.yAxes.push(new ValueAxis());
+        valueAxis.title.text = "Litres sold (M)";
+
+        LineSeries lineSeries = (LineSeries) xyChart.series.push(new LineSeries());
+        lineSeries.dataFields.valueY = "litres";
+        lineSeries.dataFields.categoryX = "country";
+        xyChart.dataSource.url = "data/basic.json";
+
+        XYChartScrollbar scrollbarX = new XYChartScrollbar();
+        scrollbarX.series.push(lineSeries);
+        xyChart.scrollbarX = scrollbarX;
+
+        // Hiding the scrollbar
+        /*scrollbarX.scrollbarChart.seriesContainer.hide();*/
     }
 }
